6c9bcf0bfd71593b95227d5e379ca0e6ddc1fb86
Kristian Perez <kpere192@fiu.edu>
2024-06-06 17:40:37 -0400

    Temporarily made tournaments the root page and made routes for that

--- MatchMasterNG/src/app/app-routing.module.ts
+++ MatchMasterNG/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TournamentComponent } from './components/tournament/tournament.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path: "tournaments", component: TournamentComponent, title: "Tournaments"},
+  {path: "", redirectTo: "tournaments", pathMatch: "full"},
+  {path: "**", redirectTo: "tournaments"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
