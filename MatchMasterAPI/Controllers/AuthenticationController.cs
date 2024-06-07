ef714593e671b642381c108a49b226fa6fe4ad83
Kristian Perez <kpere192@fiu.edu>
2024-06-07 13:09:36 -0400

    Added an authentication controller to handle login and sign up. Finished sign up for now. Password is made by hashing the password with a generated salt value that is saved in the Users table

---
+++ MatchMasterAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatchMasterAPI.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace MatchMasterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly MatchMasterContext _context;
+
+        public AuthController(MatchMasterContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User userToRegister, string password)
+        {
+            // Check if email already exists
+            if (await _context.Users.AnyAsync(u => u.Email == userToRegister.Email))
+                return BadRequest("Email is already taken");
+
+            // Generate Salt
+            byte[] salt = GenerateSalt(16);
+
+            // Hash the password
+            var passwordHash = HashPassword(password, salt);
+
+            var userToCreate = new User
+            {
+                Username = userToRegister.Username,
+                Email = userToRegister.Email,
+                HashedPassword = passwordHash
+            };
+
+            _context.Users.Add(userToCreate);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(User userToLogin)
+        {
+            return Ok();
+        }
+
+        private byte[] HashPassword(string password, byte[] salt)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] combineBytes = new byte[passwordBytes.Length + salt.Length];
+
+            using(var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(combineBytes);
+                return hashedBytes;
+            }
+        }
+
+        private byte[] GenerateSalt(int length)
+        {
+            byte[] salt = new byte[length];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+    }
+}
--- MatchMasterAPI/obj/Debug/net8.0/MatchMasterAPI.AssemblyInfo.cs
+++ MatchMasterAPI/obj/Debug/net8.0/MatchMasterAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchMasterAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bca95d40e6aab4eaab636dda2b183aee5ef383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281ebcc95d5a230523f323d1fe1d92d7a3b515c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchMasterAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchMasterAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
